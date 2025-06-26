8e7f52806446dd1ff86e71369ff916926ad7a912
sourabhaprasad <sourabhaprasad04@gmail.com>
2025-06-27 02:46:18 +0530

    fix: Ui changes

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,8 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-900 backdrop-blur-md text-center text-sm py-4 text-white">
-      © {new Date().getFullYear()} Movie Sentiment Analyzer — Built for Zero To
-      One Internship
+      © {new Date().getFullYear()} Movie Sentiment Analyzer
     </footer>
   );
 }
